0d46e1311f9bbb0bc527b558c700e83b4b03004e
Matias <analistaena@gmail.com>
2026-07-22 11:27:59 -0300

    s

--- js/config.js
+++ js/config.js
@@ -1,7 +1,5 @@
 // Variable global que detecta si corre en local o producción
-const API_URL = window.location.origin.includes('localhost') || window.location.origin.includes('127.0.0.1')
-    ? 'http://localhost:3000'
-    : window.location.origin;
+const API_URL = 'https://diagramasnode.onrender.com';
 
 // Estado global de la aplicación compartido entre módulos
 let RAM_Flota = [];
